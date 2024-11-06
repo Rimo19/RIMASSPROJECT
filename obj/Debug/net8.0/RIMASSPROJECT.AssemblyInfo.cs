8e2ce82063f9443ec7b8d8a6af647855f9d2aeaa
Noves67 <nouf23gh@gmail.com>
2024-11-06 14:05:12 +0300

    nouf name

--- obj/Debug/net8.0/RIMASSPROJECT.AssemblyInfo.cs
+++ obj/Debug/net8.0/RIMASSPROJECT.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RIMASSPROJECT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1e0f0378937fa1f9e9bf8b024d1fd2bea5e8885")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b41fb9a05165cec71a5901451099f33243df2f4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("RIMASSPROJECT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RIMASSPROJECT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
